4d730589be9911f5faed014c8e956cff1b615182
asentaja <Asentaja@BC-5CD026D1XT.bc.local>
2022-05-17 14:42:29 +0300

    modified to meet requirements for the latest task

--- NuGet/NuGet/Program.cs
+++ NuGet/NuGet/Program.cs
@@ -6,9 +6,34 @@ namespace NuGet
     {
         static void Main(string[] args)
         {
+            int amount = 0;
             int loop = 0;
 
-            while (loop < 3)
+            if (args.Length != 0)
+            {
+                if (args[0] == "interactive")
+                {
+                    Console.WriteLine("How many persons should I print?");
+                    amount = Convert.ToInt32(Console.ReadLine());
+                } 
+                else if (args[0] == "personcount")
+                {
+                    try
+                    {
+                        amount = Convert.ToInt32(args[1]);
+                    }
+                    catch
+                    {
+                        errorMessage(1);
+                    }
+                }
+            } 
+            else
+            {
+                errorMessage(0);
+            }
+
+            while (loop < amount)
             {
                 Console.WriteLine(Faker.Name.FullName());
                 Console.Write(Faker.Address.StreetAddress() + ", ");
@@ -19,6 +44,27 @@ namespace NuGet
                 Console.WriteLine();
                 Console.WriteLine();
                 loop++;
+            }
+        }
+
+        static public void errorMessage(int errorNum)
+        {
+            Console.WriteLine(errorSwitch(errorNum));
+            Console.WriteLine("Terminating Program");
+            Environment.Exit(errorNum);
+        }
+
+        static public string errorSwitch(int errorNum)
+        {
+            switch (errorNum)
+            {
+                case 0:
+                    return "No startup arguments were given:";
+                case 1:
+                    return "Invalid startup arguments:";
+                default:
+                    return "NoErrorFound/UnknownError:";
+                    
             }
         }
 
